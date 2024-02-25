09dfb5321c82f70edfd3bc72476d470d5b87f672
Erick Kopak Bonfim <156819866+Erickkbonfim@users.noreply.github.com>
2024-02-25 19:45:03 -0300

    Update middlewares _necessarios.js

--- middlewares _necessarios.js
+++ middlewares _necessarios.js
@@ -2,8 +2,14 @@
 const express = require('express')
 const router = express.Router()
 
+//para uma imagem só:
 const { imageUpload } = require('../helpers/image-upload')
 router.patch('/edit/:id', imageUpload.single("image"), userController.editUser)
+
+//para mais imagens utiliza-se o método array('images') da seguinte forma:
+router.post('/creat', imageUpload.array('images'), userController.create)
+
+
 /*
 o imageUpload é importado do helper isoladamente para ficar na rota antes do controller e leva como parametro o tipo de arquivo que recebe no caso .single("image")
 */
